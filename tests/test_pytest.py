9ae3d944c5e7119aa5e9283de3208f7ce7609f24
Samuel_Truji <samaletrumon@gmail.com>
2025-03-15 16:42:43 -0500

    finish course the unittest

---
+++ tests/test_pytest.py
@@ -0,0 +1,14 @@
+import pytest
+from src.bank_account import BankAccount
+
+
+@pytest.mark.parametrize("ammount, expected", [
+    (100, 1100),
+    (3000, 4000),
+    (4500, 5500),
+])
+def test_deposit_multiple_ammounts(ammount, expected):
+    account = BankAccount(balance=1000, log_file="transactions.txt")
+    new_balance = account.deposit(ammount)
+    assert new_balance == expected
+    
